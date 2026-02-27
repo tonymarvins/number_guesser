47a24c46ee0551a5ea531e8000c8502f7e116263
tonymarvins <tonymarvinscrewman@gmail.com>
2026-02-27 16:32:02 +0300

    Update mainv2.py
    add attempt counter and try limit

--- mainv2.py
+++ mainv2.py
@@ -17,3 +17,13 @@ elif level == "2":
 else:
   max_tries = 15
   number_to_guess = random.randint(1,200)
+
+attempts = 0
+while attempts < max_tries:
+  guess = int(input("Enter your guess: "))
+    attempts += 1
+    if guess == number_to_guess:
+        print(f"You guessed it in {attempts} tries! 🎉")
+        break
+    else:
+        print("Wrong! Try again.")
