aa0997f1cce59f18ce82ff5b1c025948867bcaa3
tonymarvins <tonymarvinscrewman@gmail.com>
2026-02-27 16:53:40 +0300

    Update mainv2.py
    implement attempts loop using modules

--- mainv2.py
+++ mainv2.py
@@ -17,17 +17,14 @@ number_to_guess, max_tries = generate_number(level)
 log(f"Game started at level {level}")
 attempts = 0
 while attempts < max_tries:
-  guess = int(input("Enter your guess: "))
+  guess = get_number("Enter your guess: "))
     attempts += 1
-    if guess == number_to_guess:
-        print(f"You guessed it in {attempts} tries! 🎉")
+  result = check_guess(guess, number_to_guess)
+    print(result)
+  if result == "Correct":
+     print(f"You guessed it in {attempts} tries! 🎉")
         break
-    else:
-        print("Wrong! Try again.")
-if guess < number_to_guess:
-  print("Too low!")
-elif guess > number_to_guess:
-  print("Too high!")
+  
 
 if attempts == max_tries and guess != number_to_guess:
     print(f"Game over! The number was {number_to_guess}")
