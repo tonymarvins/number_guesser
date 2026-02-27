4dbb34b998a07a2b6bf4b8d50bb20e519b7c5438
tonymarvins <tonymarvinscrewman@gmail.com>
2026-02-27 16:40:53 +0300

    Update game.py
    move core game logic to game.py

--- game.py
+++ game.py
@@ -5,4 +5,10 @@ from config import LEVELS
 def generate_number(level):
   rng, tries = LEVELS[level]
   return random.randint(1, rng),tries
-  
+def check_guess(guess, number):
+    if guess < number:
+        return "Too low!"
+    elif guess > number:
+        return "Too high!"
+    else:
+        return "Correct"
