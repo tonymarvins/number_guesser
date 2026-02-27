1d904f8dcac9a7ebf4317e017a24c2f044ef4584
tonymarvins <tonymarvinscrewman@gmail.com>
2026-02-27 16:11:11 +0300

    Update main.py
    check guess and give feedback

--- main.py
+++ main.py
@@ -6,3 +6,8 @@ print("Welcome to the Number Guesser Game!")
 number_to_guess = random.randit(1, 100)
 print("Guess a number between 1 and 100")
 guess = int(input("Enter your guess: "))
+
+if guess == number_to_guess:
+  print("You guessed it! 🎉")
+else:
+  print("Wrong! Try again.")
