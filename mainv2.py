f597f70bb9f59b44647c146d9e4869f92ca26bfa
tonymarvins <tonymarvinscrewman@gmail.com>
2026-02-27 16:29:08 +0300

    Update mainv2.py
    set number range and max tries based on level

--- mainv2.py
+++ mainv2.py
@@ -7,3 +7,13 @@ print("2. Medium (1-100, 7 tries)")
 print("3. Hard (1-200, 5 tries)")
 
 level = input("Choose level (1-3): ")
+
+if level == "1":
+  max_tries = 10
+  number_to_guess = random.randint(1, 50)
+elif level == "2":
+  max_tries = 12
+  number_to_guess = random.randint(1,100)
+else:
+  max_tries = 15
+  number_to_guess = random.randint(1,200)
