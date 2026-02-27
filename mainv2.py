d6c9513fcb699e319a36b218d776db52917adfa9
tonymarvins <tonymarvinscrewman@gmail.com>
2026-02-27 17:00:43 +0300

    Update mainv2.py

--- mainv2.py
+++ mainv2.py
@@ -7,8 +7,8 @@ print(f"\nWelcome to {APP_NAME}!")
 # Level selection
 print("\nSelect difficulty level:")
 print("1. Easy (1-50, 10 tries)")
-print("2. Medium (1-100, 7 tries)")
-print("3. Hard (1-200, 5 tries)")
+print("2. Medium (1-100, 12 tries)")
+print("3. Hard (1-200, 15 tries)")
 
 level = input("Choose level (1-3): ")
 #generate number and set max tries
