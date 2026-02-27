e7172949555cf0ed8755ddb9e740a969916b0ff7
tonymarvins <tonymarvinscrewman@gmail.com>
2026-02-27 16:35:11 +0300

    Update mainv2.py
    add game over message when attempts run out

--- mainv2.py
+++ mainv2.py
@@ -31,3 +31,6 @@ if guess < number_to_guess:
   print("Too low!")
 elif guess > number_to_guess:
   print("Too high!")
+
+if attempts == max_tries and guess != number_to_guess:
+    print(f"Game over! The number was {number_to_guess}")
